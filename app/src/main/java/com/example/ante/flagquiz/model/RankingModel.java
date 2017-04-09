b788bb23215992470f417172d8b6f5329bcf734a
a19p93dev@gmail.com <AP-93@users.noreply.github.com>
2017-04-09 20:14:51 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ante/flagquiz/model/RankingModel.java
@@ -0,0 +1,31 @@
+package com.example.ante.flagquiz.model;
+
+
+
+public class RankingModel {
+
+        private int Id;
+        private int Score;
+
+        public RankingModel(int id, int score) {
+            Id = id;
+            Score = score;
+        }
+
+        public int getId() {
+            return Id;
+        }
+
+        public void setId(int id) {
+            Id = id;
+        }
+
+        public int getScore() {
+            return Score;
+        }
+
+        public void setScore(int score) {
+            Score = score;
+        }
+    }
+
